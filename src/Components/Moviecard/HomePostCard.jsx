8fcc062f2f563716ba7e13c40bbc4bbaf6d30c25
jitendra <er.sainijitendra@gmail.com>
2025-07-21 17:22:17 +0530

    remove unuse click cmt

--- src/Components/Moviecard/HomePostCard.jsx
+++ src/Components/Moviecard/HomePostCard.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 function HomePostCard({ item }) {
-  const handleClick = () => {
-    console.log("Card clicked!");
-  };
+  const handleClick = () => {};
   return (
     <div
       onClick={handleClick}
