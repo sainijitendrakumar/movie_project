57b885d756f2633248d27607b9f60cddce61a49f
jitendra <er.sainijitendra@gmail.com>
2025-08-01 21:03:12 +0530

    add year date

--- src/Components/Moviecard/HomePostCard.jsx
+++ src/Components/Moviecard/HomePostCard.jsx
@@ -18,7 +18,14 @@ function HomePostCard({ item }) {
         {/* Bottom Gradient Overlay */}
         <div className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-black/40 to-transparent rounded-b-lg" />
       </div>
-
+      <div className="absolute top-1 right-2 bg-red-500 px-2 rounded-xl flex justify-center items-center">
+        <p
+          className="text-center text-[11px] font-semibold  tracking-tight text-white "
+          style={{ lineHeight: "1.2" }}
+        >
+          {item.release_date}
+        </p>
+      </div>
       {/* Title */}
       <div className="absolute bottom-3 left-4 right-4 flex justify-center items-center">
         <h2
